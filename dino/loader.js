2045ecaa28dc3471ae903d3895159b52005b20dd
Mohammad Anas <anas.1633.m@gmail.com>
2025-11-13 13:57:59 +0530

    added jump on pinch gesture

--- dino/loader.js
+++ dino/loader.js
@@ -3,8 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
   
     setTimeout(() => {
       window.startHandTracking((hand) => {
-        const palmOpen = hand[9].y < hand[0].y;
-        if (palmOpen) {
+        // console.log(hand);
+        const indexTipX = hand[8].x;
+        const indexTipY = hand[8].y;
+        const thumbTipX = hand[4].x;
+        const thumbTipY = hand[4].y;
+
+        const tipX = Math.abs(indexTipX - thumbTipX);
+        const tipY = Math.abs(indexTipY - thumbTipY);
+
+        const distance = Math.sqrt(tipX*tipX, tipY*tipY);
+
+        const pinch =  distance < 0.01;
+        if (pinch) {
           runner.tRex.startJump(0);
         }
       });
